ec4621dfff157ff85ae5fe895bb759fb8c48cd09
Russell Wilson <eyeamdigital@gmail.com>
2025-05-23 12:41:29 -0600

    created js action

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  try {
+    const firstNumber = core.getInput("number1");
+    const secondNumber = core.getInput("number2");
+
+    const result = parseInt(firstparseInt) + Number(secondNumber);
+    core.setOutput("result", result);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
